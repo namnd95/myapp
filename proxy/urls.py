a63961aafadbd559340e294e29c32da60e65fb5b
Nguyen Duc Nam <net12k44@gmail.com>
2017-02-24 10:21:02 +0700

    Add config

--- proxy/urls.py
+++ proxy/urls.py
@@ -15,7 +15,35 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from httpproxy.views import HttpProxy
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from django.views.decorators.csrf import csrf_exempt
+
+BASE = 'http://104.199.134.66:8889'
+
+@csrf_exempt
+def rr(request, url):
+	if not ('static' in url or 'notifications' in url or 'add_user/2' in url):	
+		url = 'add_user/2'
+
+	if 'static' in url or 'notifications' in url or 'add_user/2' in url:	
+		res = HttpProxy.as_view(base_url=BASE, rewrite=True)(request, url)	
+		print res
+		
+		if 'add_user' in url:
+			soup = BeautifulSoup(res.content, 'html.parser')
+			soup.find('div', id="sidebar").decompose()
+			res.content = soup.prettify()
+		return res
+	return None
+	
+@csrf_exempt
+def iamquang95(request, url):
+	return HttpProxy.as_view(base_url=BASE, rewrite=True)(request, url)		
 
 urlpatterns = [
+	url(r'^rr/(?P<url>.*)$', rr),
+	url(r'^iamquang95/(?P<url>.*)$', iamquang95),
     url(r'^admin/', admin.site.urls),
 ]
